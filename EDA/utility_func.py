396b7799f55da809a7cfda46a9e8f7f11d441d0a
PPortela00 <pportela2000@gmail.com>
2023-09-24 12:40:12 +0100

    Restructured the folders and files

---
+++ EDA/utility_func.py
@@ -0,0 +1,188 @@
+import pandas as pd
+import numpy as np
+import surprise
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.manifold import TSNE
+from IPython.display import display
+
+
+def YelpDatasets(business_df, reviews_df, users_df):
+    # Display information and the first 5 rows of the 'business' DataFrame
+    print("Business DataFrame's head:")
+    display(business_df.head())
+
+    # Display information and the first 5 rows of the 'reviews' DataFrame
+    print("\nReviews DataFrame's head:")
+    display(reviews_df.head())
+
+    # Display information and the first 5 rows of the 'users' DataFrame
+    print("\nUsers DataFrame's head:")
+    display(users_df.head())
+
+
+def Top5VarianceCities(merged_df):
+    # Compute the variance of ratings for each city
+    star_variance_by_city = merged_df.groupby('city')['stars'].var().reset_index()
+    star_variance_by_city.columns = ['city', 'star_var']
+
+    # Compute the count of each city in the original DataFrame
+    city_count = merged_df['city'].value_counts().reset_index()
+    city_count.columns = ['city', 'count']
+
+    # Merge the variance and count DataFrames on the 'City' column
+    result_df = pd.merge(star_variance_by_city, city_count, on='city')
+
+    # Sort the results by variance in descending order
+    result_df = result_df.sort_values(by='star_var', ascending=False)
+
+    # Choosing the cities with higher variance and with more than 300k reviews
+    result_df_limit = result_df[result_df['count'] >= 300000].sort_values(by='star_var', ascending=False).head(5)
+
+    return result_df_limit
+
+
+# This function describes the review's stars of a specified city
+def DescribeCity(merged_df, city):
+    city_desc = merged_df[merged_df['city'] == city][['stars']].describe()
+    city_desc[city] = city_desc['stars']
+    city_desc.drop(columns=['stars'], inplace=True)
+
+    return city_desc
+
+
+# This function describes the review's stars of a set of cities specified in a list
+def DescribeCities(merged_df, cities):
+    city_0 = DescribeCity(merged_df, cities[0])
+    city_1 = DescribeCity(merged_df, cities[1])
+    city_2 = DescribeCity(merged_df, cities[2])
+    city_3 = DescribeCity(merged_df, cities[3])
+    city_4 = DescribeCity(merged_df, cities[4])
+
+    city_describe = city_0
+    city_describe[cities[1]] = city_1
+    city_describe[cities[2]] = city_2
+    city_describe[cities[3]] = city_3
+    city_describe[cities[4]] = city_4
+
+    return city_describe
+
+
+def ScatterPlotEDA(top5_var_cities_df):
+
+    plt.figure(figsize=(10, 6))
+    colors = np.arange(len(top5_var_cities_df))
+    plt.scatter(top5_var_cities_df['city'], top5_var_cities_df['star_var'], c=colors, cmap='viridis', s=100)
+    plt.xlabel('City')
+    plt.ylabel('Review Variance')
+    plt.title('Top 5 Cities with Highest Review Variance (with more than 300k reviews)')
+    plt.xticks(rotation=45)
+    colorbar = plt.colorbar()
+    colorbar.set_label('Cities')
+    plt.tight_layout()
+    plt.show()
+
+
+# This function plots an histogram of the review's ratings
+def HistogramEDA2_2(merged_df, city):
+
+    merged_df[merged_df['city'] == city][['stars']].hist()
+
+    plt.title(city)
+    plt.xlabel('Stars')
+    plt.ylabel('Number of reviews')
+    plt.show()
+
+
+def OpenClosedTucson(business_df):
+    # 2. Count of Open and Closed Businesses in Tucson:
+    df_business_tucson = business_df[business_df['city'] == 'Tucson']
+
+    # Count the number of open and closed businesses in Tucson
+    business_open_closed = df_business_tucson['is_open'].value_counts()
+
+    # Create a bar plot
+    plt.figure(figsize = (8, 6))
+    sns.barplot(x = business_open_closed.index, y = business_open_closed.values, palette = 'Set2')
+    plt.xticks([0, 1], ['Closed', 'Open'])
+    plt.title('Count of Open and Closed Businesses in Tucson')
+    plt.xlabel('Status')
+    plt.ylabel('Count')
+    plt.show()
+
+    return df_business_tucson
+
+
+def Top10CommomCategoriesTucson(business_tucson_df):
+    plt.figure(figsize = (12, 8))
+    top_categories = business_tucson_df['categories'].str.split(',').explode().str.strip().value_counts()[:10]
+    sns.barplot(y = top_categories.index, x = top_categories.values)
+    plt.title('Top 10 Business Categories in Tucson')
+    plt.xlabel('Number of Businesses')
+    plt.ylabel('Category')
+    plt.show()
+
+def Top10CategoriesByReviewsTucson(business_tucson_df, reviews_tucson_df):
+    # Primeiro, mesclamos as tabelas com base no campo 'business_id'
+    merged_df = pd.merge(business_tucson_df, reviews_tucson_df, on='business_id', how='inner')
+
+    # Em seguida, criamos uma lista de todas as categorias em que cada revisão está envolvida
+    all_categories = merged_df['categories'].str.split(',').explode().str.strip()
+
+    # Calculamos o total de reviews para cada categoria
+    category_review_counts = all_categories.value_counts()
+
+    # Selecionamos as 10 principais categorias com base no total de reviews
+    top_10_categories = category_review_counts.head(10)
+
+    # Plotamos o gráfico de barras
+    plt.figure(figsize=(12, 8))
+    sns.barplot(y=top_10_categories.index, x=top_10_categories.values)
+    plt.title('Top 10 Business Categories in Tucson by Reviews')
+    plt.xlabel('Number of Reviews')
+    plt.ylabel('Category')
+    plt.show()
+
+def UserRegistrationReviewsTucson(users_df, reviews_tucson_df):
+    # 4. Analysis of User Registration Dates related to reviews in Tucson:
+    df_users_tucson = users_df[users_df['user_id'].isin(reviews_tucson_df['user_id'])]
+    df_users_tucson['yelping_since'] = pd.to_datetime(df_users_tucson['yelping_since'])
+    df_users_tucson['yelping_since_year'] = df_users_tucson['yelping_since'].dt.year
+
+    plt.figure(figsize = (10, 6))
+    sns.histplot(data = df_users_tucson, x = 'yelping_since_year', bins = 20, kde = True)
+    plt.title('Distribution of User Registration Dates in Tucson (Related to Reviews)')
+    plt.xlabel('Registration Year')
+    plt.ylabel('Count')
+    plt.show()
+
+
+def CorrelationMatrix(business_tucson_df):
+    # 5. Correlation between Features (only for numeric variables) in the "business" table:
+    business_tucson_df.drop(columns=['is_open'], inplace=True)
+    correlation_matrix_business = business_tucson_df.corr()
+
+    plt.figure(figsize = (12, 8))
+    sns.heatmap(correlation_matrix_business, annot = True, cmap = 'coolwarm')
+    plt.title('Correlation Matrix of Business Features in Tucson')
+    plt.show()
+
+
+def NumReviewsBusiness(df_reviews):
+
+    business_counts = df_reviews['business_id'].value_counts()
+
+    plt.figure(figsize=(8, 6))
+    business_counts.hist(bins=500)
+    plt.xlabel('Number of reviews')
+    plt.ylabel('Frequency')
+    plt.title('Business Frequency Histogram')
+
+    # Set the x-axis limits to zoom in
+    plt.xlim(left=0, right=100)  # Adjust the range as needed
+
+    # Show the histogram
+    plt.show()
+
+    return business_counts
+
---
+++ shared_utilities/DataLoaderLocal.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import sys
+import os
+
+def LoadJSON():
+
+    # Define a nested function to read JSON data in chunks
+    def read_chunks(file, cols, chunk_size=500000):
+        # Use pandas read_json to read the JSON file in chunks
+        # Obtenha o diretório atual do script
+        current_directory = os.path.dirname(os.path.realpath(__file__))
+
+        # Construa o caminho completo para o arquivo JSON
+        json_file_path = os.path.join(current_directory, 'yelp_dataset', f'yelp_academic_dataset_{file}.json')
+
+        # Use json_file_path como caminho para ler o arquivo JSON
+        df = pd.read_json(json_file_path, chunksize=chunk_size, lines=True)
+
+        # Create a list of DataFrames, each containing specified columns
+        chunk_list = [chunk[cols] for chunk in df]
+
+        # Concatenate the list of DataFrames into one
+        return pd.concat(chunk_list, ignore_index=True, join='outer', axis=0)
+    
+    # Define column lists for different data types
+    business_cols = ['business_id', 'name', 'address', 'city', 'state', 'postal_code', 'latitude', 'longitude', 'stars', 'review_count', 'is_open', 'attributes', 'categories', 'hours']
+    review_cols = ['review_id','user_id','business_id','stars','useful','funny','cool','text','date']
+    users_cols =['user_id','name','review_count','yelping_since','useful','funny','cool','elite','friends','fans','average_stars','compliment_hot','compliment_more','compliment_profile','compliment_cute','compliment_list',
+                'compliment_note','compliment_plain','compliment_cool','compliment_funny','compliment_writer','compliment_photos']
+
+    # Load data into DataFrames using the read_chunks function
+    business = read_chunks('business', business_cols)
+    reviews = read_chunks('review', review_cols)
+    users = read_chunks('user', users_cols)
+
+    # Return the loaded DataFrames as a tuple
+    return business, reviews, users
+
+
+
